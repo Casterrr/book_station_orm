1a67549ff9a968f0a88a3eb0a48b9a4c292b5f84
Lucas Matheus <lucmvl2003@gmail.com>
2024-07-21 00:34:46 -0300

    init project

---
+++ src/test/java/cacadores/ifal/poo/book_station/BookStationApplicationTests.java
@@ -0,0 +1,13 @@
+package cacadores.ifal.poo.book_station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
