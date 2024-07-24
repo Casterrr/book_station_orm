387c4c83f9029ffd44d2538149ce2568733c2c4f
Filipe Zaidan <felipezaidan10@gmail.com>
2024-07-23 23:33:39 -0300

    feat: add data faker population

---
+++ src/main/java/cacadores/ifal/poo/book_station/util/DataPopulationCommand.java
@@ -0,0 +1,27 @@
+package cacadores.ifal.poo.book_station.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("!production")
+public class DataPopulationCommand implements CommandLineRunner {
+    private final DataPopulator dataPopulator;
+
+    @Autowired
+    public DataPopulationCommand(DataPopulator dataPopulator) {
+        this.dataPopulator = dataPopulator;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (args.length > 0 && args[0].equals("populate")) {
+            System.out.println("Starting data population...");
+            dataPopulator.run();
+            System.out.println("Data population completed successfully.");
+
+        }
+    }
+}
